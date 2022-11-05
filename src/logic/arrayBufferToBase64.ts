2863161b739f88ca2484ec40dbaafed97cec40ce
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-11-05 09:56:42 +0700

    use js-base64

--- src/logic/arrayBufferToBase64.ts
+++ src/logic/arrayBufferToBase64.ts
@@ -1,9 +1,12 @@
-export function arrayBufferToBase64( buffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
-    }
-    return btoa( binary );
+import { btoa } from "js-base64"
+
+export function arrayBufferToBase64(buffer) {
+  // eslint-disable-next-line functional/no-let
+  let binary = ""
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  // eslint-disable-next-line functional/no-let
+  for (let i = 0; i < len; i++) binary += String.fromCharCode(bytes[i])
+
+  return btoa(binary)
 }
--- src/logic/base64ToArrayBuffer.ts
+++ src/logic/base64ToArrayBuffer.ts
@@ -1,9 +1,15 @@
+import { atob } from "js-base64"
+
 export function base64ToArrayBuffer(base64) {
-    var binary_string = window.atob(base64);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
-    }
-    return bytes.buffer;
+  // eslint-disable-next-line camelcase
+  const binary_string = atob(base64)
+  // eslint-disable-next-line camelcase
+  const len = binary_string.length
+  const bytes = new Uint8Array(len)
+  // eslint-disable-next-line functional/no-let
+  for (let i = 0; i < len; i++)
+    // eslint-disable-next-line functional/immutable-data, camelcase
+    bytes[i] = binary_string.charCodeAt(i)
+
+  return bytes.buffer
 }
