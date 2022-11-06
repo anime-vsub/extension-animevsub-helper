5d85d40373533fe017ac227f6a519ba5921e0ca5
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-11-06 10:28:20 +0000

    update scripts release all

--- scripts/update-version-root.ts
+++ scripts/update-version-root.ts
@@ -5,15 +5,10 @@ import packageJson from "../package.json"
 import max from "semver/ranges/max-satisfying"
 import { resolve } from "path"
 import fs from "fs"
-import { execSync } from "child_process"
 
 packageJson.version = max([versionV2, versionV3], "")!
 
 fs.writeFileSync(
   resolve(__dirname, "../package.json"),
   JSON.stringify(packageJson, null, 2)
-)
-
-execSync(
-  `bumpp ${resolve(__dirname, "../package.json")} --commit --tags --no-verify`
 )
