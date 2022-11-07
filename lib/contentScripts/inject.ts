1e5ddec0e1a9f5917e8dce0882ba6175996bd956
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-11-07 08:50:42 +0000

    add version

--- lib/contentScripts/inject.ts
+++ lib/contentScripts/inject.ts
@@ -1,12 +1,12 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
+import { version } from "../../package.json"
 import type { OptionsHttpGet, OptionsHttpPost } from "../background"
 import { randomUUID } from "../logic/randomUUID"
 
 import type { DetailCustomEvent_sendToInject } from "."
 
-export interface ClientOptionHttpGet
-  extends Omit<OptionsHttpGet, "signalId"> {
+export interface ClientOptionHttpGet extends Omit<OptionsHttpGet, "signalId"> {
   signal?: AbortSignal
 }
 export interface ClientOptionHttpPost
@@ -24,7 +24,9 @@ function createPorter(
 ) {
   return new Promise((resolve, reject) => {
     const id = randomUUID()
-    const handler = (({ detail }: CustomEvent<DetailCustomEvent_sendToInject>) => {
+    const handler = (({
+      detail
+    }: CustomEvent<DetailCustomEvent_sendToInject>) => {
       if (detail.id === id) {
         if (detail.ok) resolve(detail.res)
         else reject(detail.res)
@@ -43,5 +45,6 @@ function createPorter(
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, functional/immutable-data
 ;(window as any).Http = {
   get: (options: ClientOptionHttpGet) => createPorter("get", options),
-  post: (options: ClientOptionHttpPost) => createPorter("post", options)
+  post: (options: ClientOptionHttpPost) => createPorter("post", options),
+  version
 }
