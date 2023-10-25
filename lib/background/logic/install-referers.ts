faba89a60f857f7fb44f254848e6464382a91975
Tachibana Shin <tachibshin@duck.com>
2023-10-25 22:11:24 +0700

    fix: not re-install referrer free memory

--- lib/background/logic/install-referers.ts
+++ lib/background/logic/install-referers.ts
@@ -1,14 +1,21 @@
 import { createRule } from "./create-rule"
 import { installRules } from "./install-rules"
 
+const referrersInstalled = new Set<string>()
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const NOOP = () => {}
+
 export function installReferrer(name: string, value: string) {
+  if (referrersInstalled.has(name)) return NOOP
+  referrersInstalled.add(name)
+
   return installRules(createRule(name, value))
 }
 export function installReferrers(referers: Record<string, string>) {
   const rules = Object.entries(referers).reduce((rules, [name, value]) => {
     rules.push(...createRule(name, value))
     return rules
-  // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-undef
   }, [] as chrome.declarativeNetRequest.Rule[])
   console.log(rules)
   return installRules(rules)
